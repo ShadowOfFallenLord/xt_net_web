cf5ecc157788cb81457b3b95887999981bbe631e
ShadowOfFallenLord <ShadowOfFallenLord@users.noreply.github.com>
2019-11-19 22:15:54 +0400

    Add Task 1.1

---
+++ xt_net_web_hw/HW_Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1.1");
+            Task_1_1.Task();
+            Console.Clear();
+        }
+    }
+}
---
+++ xt_net_web_hw/HW_Task_01/Task_1_1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_01
+{
+    class Task_1_1
+    {
+        private static bool CheckOnPositive(int num)
+        {
+            if (num > 0) return true;
+
+            Console.WriteLine(" Error!");
+            Console.WriteLine($" {num} is not positive.");
+            Console.WriteLine(" Repeat input please.");
+
+            return false;
+        }
+
+        private static int Input()
+        {
+            string input;
+            int res;
+
+            while (true)
+            {
+                Console.Write("> ");
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out res)) continue;
+
+                if (!CheckOnPositive(res)) continue;
+
+                break;
+            }
+
+            return res;
+        }
+
+        public static void Task()
+        {
+            Console.WriteLine(" Enter positive integer width:");
+            int a = Input();
+            Console.WriteLine(" Enter positive integer height:");
+            int b = Input();
+            Console.WriteLine($" Area: {a * b}");
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
