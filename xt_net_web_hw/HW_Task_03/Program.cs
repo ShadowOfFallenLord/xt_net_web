f11d9859453cde1a500ea34f62a6bfc23ec58df9
ShadowOfFallenLord <ShadowOfFallenLord@users.noreply.github.com>
2019-12-03 23:20:35 +0400

    add task 3.1

---
+++ xt_net_web_hw/HW_Task_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1.Task();
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ xt_net_web_hw/HW_Task_03/Task_1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_03
+{
+    class Task_1
+    {
+        private static int ReadInt()
+        {
+            int res = 0;
+            Console.WriteLine("Enter positive integer N:");
+            while (true)
+            {
+                Console.Write("> ");
+                if (int.TryParse(Console.ReadLine(), out res) && res > 0) break;
+                Console.WriteLine("Input Error! Enter positive integer.");
+            }
+            return res;
+        }
+
+        private static void PrintQueue<T>(Queue<T> queue)
+        {
+            foreach (T i in queue)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Task()
+        {
+            int n = ReadInt();            
+
+            Queue<int> queue = new Queue<int>(n);
+            for (int i = 1; i <= n; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            PrintQueue(queue);
+
+            while (queue.Count > 1)
+            {
+                int cnt = queue.Count;
+                for(int i = 0, t; i < cnt; i++)
+                {
+                    t = queue.Dequeue();
+                    if (i % 2 == 1) continue;
+                    queue.Enqueue(t);
+                }
+
+                PrintQueue(queue);
+            }
+        }
+    }
+}
