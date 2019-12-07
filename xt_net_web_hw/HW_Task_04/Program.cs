3b3398d9dff0744fa579c596242d187ff32f6998
ShadowOfFallenLord <ShadowOfFallenLord@users.noreply.github.com>
2019-12-07 22:33:00 +0400

    Add task 4.1-4.2

---
+++ xt_net_web_hw/HW_Task_04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1_2.Task();
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ xt_net_web_hw/HW_Task_04/Task_1_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task_04
+{
+    class Task_1_2
+    {
+        static void PrintArray<T>(IEnumerable<T> list)
+        {
+            foreach (T i in list)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void SortArray<T>(T[] array, Func<T, T, bool> is_less)
+        {
+            int l = array.Length;
+            for(int i = 1, j = 0; i < l; i++)
+            {
+                j = i;
+                while(j > 0 && is_less(array[j], array[j - 1]))
+                {
+                    T t = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = t;
+                    j--;
+                }
+            }
+        }
+
+        public static void Task()
+        {
+            string[] a = new string[] {
+                "aaaaa",
+                "a",
+                "aaaa",
+                "aa",
+                "aaa", };
+
+            PrintArray(a);
+
+            SortArray(a, (x, y) => (x.Length < y.Length));
+
+            PrintArray(a);
+        }
+    }
+}
